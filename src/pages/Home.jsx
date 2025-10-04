7fea1f64aeaae72e13c09d4e4f35cd289412384f
Hayden Winkler <99040410+Haywinkler05@users.noreply.github.com>
2025-10-04 17:32:19 -0500

    Implemented a nav bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { ThemeToggle } from "../components/ThemeToggle"
 import { StarBackground } from "../components/StarBackground"
+import { NavBar } from "../components/Navbar"
 
 export const Home = () => {
     return <div className=" min-h-screen bg-background text-foreground overflow-x-hidden">
@@ -10,7 +11,7 @@ export const Home = () => {
         <StarBackground />
 
         {/*Nav bar */}
-
+        <NavBar />
 
         {/* Main Content */}
 
